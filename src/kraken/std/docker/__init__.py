7d967457314f385d29fa9bb58353c007ec7b626f
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-07-04 17:03:28 +0200

    expose DockerBuildTask in kraken.std.docker module

--- src/kraken/std/docker/__init__.py
+++ src/kraken/std/docker/__init__.py
@@ -1,4 +1,4 @@
-from .tasks import docker_build as build
+from .tasks import docker_build as build, DockerBuildTask
 
 __version__ = "0.1.0"
-__all__ = ["build"]
+__all__ = ["build", "DockerBuildTask"]
