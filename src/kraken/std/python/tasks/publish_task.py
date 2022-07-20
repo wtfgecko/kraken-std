56649c5b51e29cbbab2c90284534d8b091333f95
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-07-20 14:22:24 +0200

    add missing imports

--- src/kraken/std/python/tasks/publish_task.py
+++ src/kraken/std/python/tasks/publish_task.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 
 from kraken.core import Project, Property, Task, TaskResult
 from twine.commands.upload import upload as twine_upload
--- src/kraken/std/python/tasks/pytest_task.py
+++ src/kraken/std/python/tasks/pytest_task.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Any
+from typing import Any, List
 
 from kraken.core import Project, Property, TaskResult
 from kraken.core.utils import flatten
