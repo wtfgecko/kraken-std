299f222dcebd3d248f66ccb23c0cfa8bf0ef8cff
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-07-22 10:57:54 +0200

    fix: Python install task now understands the `PythonSettings.always_use_managed_env` option

--- src/kraken/std/python/tasks/base_task.py
+++ src/kraken/std/python/tasks/base_task.py
@@ -7,6 +7,7 @@ import subprocess as sp
 from typing import Iterable, MutableMapping
 
 from kraken.core import Project, Task, TaskRelationship, TaskResult
+from nr.python.environment.virtualenv import VirtualEnvInfo, get_current_venv
 
 from kraken.std.python.buildsystem import ManagedEnvironment
 
@@ -38,16 +39,14 @@ class EnvironmentAwareDispatchTask(Task):
         return TaskResult.from_exit_code(code)
 
     def activate_managed_environment(self, pyenv: ManagedEnvironment, envvar: MutableMapping[str, str]) -> None:
-        active_venv = envvar.get("VIRTUAL_ENV", "") or None
+        active_venv = get_current_venv(envvar)
         if active_venv is None or self.settings.always_use_managed_env:
             if not pyenv.exists():
                 logger.warning("Managed environment (%s) does not exist", pyenv)
                 return
-            env_path = pyenv.get_path()
-            logger.info("Activating managed environment (%s)", env_path)
-            bin_dir = env_path / ("Scripts" if os.name == "nt" else "bin")
-            envvar["VIRTUAL_ENV"] = str(env_path)
-            envvar["PATH"] = os.pathsep.join([str(bin_dir), envvar["PATH"]])
+            managed_env = VirtualEnvInfo(pyenv.get_path())
+            logger.info("Activating managed environment (%s)", managed_env.path)
+            managed_env.activate()
         elif active_venv:
             logger.info("An active virtual environment was found, not activating managed environment")
 
--- src/kraken/std/python/tasks/install_task.py
+++ src/kraken/std/python/tasks/install_task.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 import logging
+import os
 from typing import Optional, Union, cast
 
 from kraken.core import Project, Property, Supplier, Task, TaskResult
+from nr.python.environment.virtualenv import get_current_venv
 
 from ..buildsystem import PythonBuildSystem
 from ..settings import python_settings
@@ -13,8 +15,11 @@ logger = logging.getLogger(__name__)
 
 class InstallTask(Task):
     build_system: Property[Optional[PythonBuildSystem]]
+    always_use_managed_env: Property[bool]
 
     def is_skippable(self) -> bool:
+        if not self.always_use_managed_env.get() and get_current_venv(os.environ):
+            return True
         build_system = self.build_system.get()
         if build_system:
             return not build_system.supports_managed_environments()
@@ -49,5 +54,6 @@ def install(project: Project | None = None) -> InstallTask:
     if task is None:
         task = project.do(name, InstallTask, default=False)
         task.build_system.set(Supplier.of_callable(lambda: python_settings(project).build_system))
+        task.always_use_managed_env.set(Supplier.of_callable(lambda: python_settings(project).always_use_managed_env))
 
     return task
